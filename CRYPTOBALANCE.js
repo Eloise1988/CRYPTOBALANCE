e1d7540ffe5a4f8280d75baf356485e6bc8b9c89
Eloise1988 <53000607+Eloise1988@users.noreply.github.com>
2019-09-06 11:05:36 +0200

    Update CRYPTOBALANCE.js

--- CRYPTOBALANCE.js
+++ CRYPTOBALANCE.js
@@ -36,7 +36,7 @@ function CRYPTOBALANCE(ticker,address, refresh_cell){
     //
 
     ticker=ticker.toUpperCase();
-    url="http://charmantadvisory.com:5000/apiblock/"+ticker+"/"+address;
+    url="http://charmantadvisory.com/apiblock/"+ticker+"/"+address;
     var res = UrlFetchApp.fetch(url);
     var content = res.getContentText();
 
@@ -65,7 +65,7 @@ function CRYPTOLENDING(exchange,ticker,side,refresh_cell){
     ticker=ticker.toUpperCase();
     exchange=exchange.toUpperCase();
     side=side.toUpperCase();
-    url="http://charmantadvisory.com:5000/api/APR/"+exchange+"/"+ticker+"/"+side;
+    url="http://charmantadvisory.com/api/APR/"+exchange+"/"+ticker+"/"+side;
     var res = UrlFetchApp.fetch(url);
     var content = res.getContentText();
 
